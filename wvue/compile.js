ff683299248f96dce964da3da5db711f87eb23d4
wujunlong <yearthmain@gmail.com>
2020-08-06 16:03:43 +0800

    双向绑定

--- wvue/compile.js
+++ wvue/compile.js
@@ -1,68 +1,68 @@
-import compileUtil from "./utils.js";
+import compileUtil from './utils.js'
 
 class Compile {
   constructor(el, wm) {
-    this.el = this._isElementNode(el) ? el : document.querySelector(el);
-    this.wm = wm;
+    this.el = this._isElementNode(el) ? el : document.querySelector(el)
+    this.wm = wm
 
-    const fragment = this.node2Fragment(this.el);
-    this.compile(fragment);
-    this.el.append(fragment);
+    const fragment = this.node2Fragment(this.el)
+    this.compile(fragment)
+    this.el.append(fragment)
   }
 
   compileElementNode(node) {
-    const attrs = node.attributes;
-    [...attrs].forEach(attr => {
-      const { name, value } = attr;
+    const attrs = node.attributes
+    ;[...attrs].forEach((attr) => {
+      const { name, value } = attr
       if (this._isDirective(name)) {
-        const [, directive] = name.split("-");
-        const [dir, event] = directive.split(":");
-        compileUtil[dir](node, value, this.wm, event);
-        node.removeAttribute(`v-${directive}`);
+        const [, directive] = name.split('-')
+        const [dir, event] = directive.split(':')
+        compileUtil[dir](node, value, this.wm, event)
+        node.removeAttribute(`v-${directive}`)
       }
-    });
+    })
   }
 
   compileTextNode(node) {
-    const txt = node.textContent;
-    const reg = /\{\{(.*)\}\}/;
+    const txt = node.textContent
+    const reg = /\{\{(.*)\}\}/
 
     if (reg.test(txt)) {
-      compileUtil["text"](node, RegExp.$1.trim(), this.wm);
+      compileUtil['text'](node, RegExp.$1.trim(), this.wm)
     }
   }
 
   compile(node) {
-    const childNodes = node.childNodes;
-    [...childNodes].forEach(child => {
+    const childNodes = node.childNodes
+    ;[...childNodes].forEach((child) => {
       if (this._isElementNode(child)) {
-        this.compileElementNode(child);
+        this.compileElementNode(child)
       } else {
-        this.compileTextNode(child);
+        this.compileTextNode(child)
       }
       // get all childnodes
       if (child.childNodes && child.childNodes.length) {
-        this.compile(child);
+        this.compile(child)
       }
-    });
+    })
   }
 
   node2Fragment(el) {
-    const f = document.createDocumentFragment();
-    let firstChild;
+    const f = document.createDocumentFragment()
+    let firstChild
     while ((firstChild = el.firstChild)) {
-      f.append(firstChild);
+      f.append(firstChild)
     }
-    return f;
+    return f
   }
 
   _isDirective(name) {
-    return name.startsWith("v-");
+    return name.startsWith('v-')
   }
 
   _isElementNode(node) {
-    return node.nodeType === 1;
+    return node.nodeType === 1
   }
 }
 
-export default Compile;
+export default Compile
--- wvue/utils.js
+++ wvue/utils.js
@@ -1,49 +1,72 @@
-import Watcher from "./watcher.js";
+import Watcher from './watcher.js'
 
+// 暗号：老师：西瓜西瓜我是冬瓜
+// 暗号：助教：冬瓜冬瓜我是西瓜
 const compileUtil = {
   text(node, exp, wm) {
-    this.bind(node, exp, wm, "text");
+    this.bind(node, exp, wm, 'text')
   },
 
   html(node, exp, wm) {
-    this.bind(node, exp, wm, "html");
+    this.bind(node, exp, wm, 'html')
   },
 
+  // ? 实现双向数据绑定
   module(node, exp, wm) {
-    this.bind(node, exp, wm, "module");
+    this.bind(node, exp, wm, 'module')
+    // 获取现在的 value 值
+    const curValue = this.getValue(exp, wm)
+    // 添加 input 事件监听
+    node.addEventListener('input', (e) => {
+      const newValue = e.target.value
+      // 当新值和当前值不一样的时候，触发修改
+      if (curValue !== newValue) {
+        this.setValue(exp, wm, newValue)
+      }
+    })
   },
 
   on(node, exp, wm, event) {
-    const fn = wm.$options.methods && wm.$options.methods[exp];
-    node.addEventListener(event, fn.bind(wm), false);
+    const fn = wm.$options.methods && wm.$options.methods[exp]
+    node.addEventListener(event, fn.bind(wm), false)
   },
 
   bind(node, exp, wm, dir) {
-    const value = this.getValue(exp, wm);
-    const updaterFn = updaterUtil[dir];
-    updaterFn && updaterFn(node, value);
-    new Watcher(wm, exp, value => {
-      updaterFn && updaterFn(node, value);
-    });
+    const value = this.getValue(exp, wm)
+    const updaterFn = updaterUtil[dir]
+    updaterFn && updaterFn(node, value)
+    new Watcher(wm, exp, (value) => {
+      updaterFn && updaterFn(node, value)
+    })
   },
 
   getValue(exp, wm) {
-    return exp.split(".").reduce((d, c) => {
-      return d[c];
-    }, wm.$data);
-  }
-};
+    return exp.split('.').reduce((d, c) => d[c], wm.$data)
+  },
+
+  setValue(exp, wm, value) {
+    exp.split('.').forEach((k, i) => {
+      if (i < exp.length - 1) {
+        wm = wm[k]
+      } else {
+        wm[k] = value
+      }
+    })
+  },
+}
 
 const updaterUtil = {
   text(node, value) {
-    node.textContent = value;
+    node.textContent = value
   },
   html(node, value) {
-    node.innerHTML = value;
+    node.innerHTML = value
   },
   module(node, value) {
-    node.value = value;
-  }
-};
+    console.log({ node, value })
 
-export default compileUtil;
+    node.value = value
+  },
+}
+
+export default compileUtil
